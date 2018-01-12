6fefec046b87f2caa8eaf69e99f9adbcb59ee81b
mjt <mjt@SWLPC33.swl.local>
2018-01-12 16:53:05 +0000

    add extra days & items

--- php/src/texttest_fixture.php
+++ php/src/texttest_fixture.php
@@ -1,3 +1,4 @@
+#!/usr/local/bin/php
 <?php
 
 require_once 'gilded_rose.php';
@@ -7,6 +8,7 @@ echo "OMGHAI!\n";
 $items = array(
     new Item('+5 Dexterity Vest', 10, 20),
     new Item('Aged Brie', 2, 0),
+    new Item('Aardvark', 2, 10),
     new Item('Elixir of the Mongoose', 5, 7),
     new Item('Sulfuras, Hand of Ragnaros', 0, 80),
     new Item('Sulfuras, Hand of Ragnaros', -1, 80),
@@ -14,13 +16,14 @@ $items = array(
     new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49),
     new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49),
     // this conjured item does not work properly yet
-    new Item('Conjured Mana Cake', 3, 6)
+    new Item('Conjured Mana Cake', 3, 6),
+    new Item('Conjured Eagle', 3, 20)
 );
 
 $app = new GildedRose($items);
 
-$days = 2;
-if (count($argv) > 1) {
+$days = 10;
+if ((isset($argv)) && (count($argv) > 1)) {
     $days = (int) $argv[1];
 }
 
